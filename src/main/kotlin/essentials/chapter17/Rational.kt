1463c21d19d2d2e3592cb28fae97e7d7f57b537b
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-08-27 22:49:00 +0900

    chapter17

---
+++ src/main/kotlin/essentials/chapter17/Rational.kt
@@ -0,0 +1,10 @@
+package essentials.chapter17
+
+data class Rational(
+    val numerator: Int,
+    val denominator: Int,
+)
+
+fun Int.r(): Rational = Rational(this, 1)
+
+fun Pair<Int, Int>.r(): Rational = Rational(this.first, this.second)
